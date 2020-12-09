61bb6fb6381cf6ca1a05cd4cb320b4cb0e9bc074
Jair Neri <yayodcv@gmail.com>
2020-12-09 13:05:40 -0600

    conect styles and responsive

---
+++ src/components/Layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Layou = () => (
+  <div className="layout">
+    <section className="principal">
+      <div className="b1">
+        <div className="skills">1</div>
+        <div className="hobbies">2</div>
+      </div>
+      <div className="b1">
+        <div className="blog">3</div>
+        <div className="exp">5</div>
+      </div>
+    </section>
+    <div className="stats" />
+    <div className="proyects" />
+    <div className="buttons" />
+  </div>
+);
+
+export default Layou;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Router } from '@reach/router';
+
+import '../styles/mobile.css';
+import '../styles/tablet.css';
+import '../styles/desktop.css';
+import Layout from '../components/layout';
+
+const Dash = () => <div>Dash</div>;
 
 const App = () => (
-  <h1> Welcome al home</h1>
-  )
+  <>
+    <Router>
+      <Layout path="/" />
+      <Dash path="dashboard" />
+    </Router>
+  </>
+);
 
 export default App;
