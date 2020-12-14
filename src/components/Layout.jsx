091b183fd39c4f9aa1a3644b6cb88fbf8be3d211
Jair Neri <yayodcv@gmail.com>
2020-12-14 00:07:42 -0600

    Setup grid layout , ready.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 const Layou = () => (
   <div className="layout">
-    <section className="principal">
-      <div className="b1">
-        <div className="skills">1</div>
-        <div className="hobbies">2</div>
-      </div>
-      <div className="b1">
-        <div className="blog">3</div>
-        <div className="exp">5</div>
-      </div>
-    </section>
+    <div className="b1">
+      <div className="skills">1</div>
+      <div className="hobbies">2</div>
+    </div>
+    <div className="b2">
+      <div className="blog">3</div>
+      <div className="exp">5</div>
+    </div>
     <div className="stats" />
     <div className="proyects" />
     <div className="buttons" />
