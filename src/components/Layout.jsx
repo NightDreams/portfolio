558f8177919ff54193ae4ac05207bc989a1600a2
Jair Neri <yayodcv@gmail.com>
2020-12-15 00:35:38 -0600

    Components skills - elemenets ready. setup fonts -public index.html

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Layou = () => (
   <div className="layout">
     <div className="b1">
-      <div className="skills">1</div>
+      <div className="skills">
+        <h2>front end</h2>
+        <div className="set">
+          <h3>tecnologia</h3>
+          <div className="barlevel">
+            <div className="progress">70</div>
+          </div>
+        </div>
+      </div>
       <div className="hobbies">2</div>
     </div>
     <div className="b2">
