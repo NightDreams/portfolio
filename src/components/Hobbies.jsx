8c4c3c72835ec0e7e2192714643cf3d2969ea19d
Jair Neri <yayodcv@gmail.com>
2020-12-23 20:18:35 -0600

    Hobbies setup structure, styles

--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+const Hobbie = () => (
+  <div className="h-e">
+    <picture>
+      <img src="" alt="hobbiX" />
+    </picture>
+    <h3>Gaming</h3>
+    <p>Quisque feugiat malesuada molestie.</p>
+  </div>
+);
+
 const Hobbies = () => (
   <>
-    <div className="hobbies">2</div>
+    <div className="hobbies">
+      <h2>Hobbies</h2>
+      <div className="h-l">
+        <Hobbie />
+        <Hobbie />
+        <Hobbie />
+      </div>
+    </div>
   </>
 );
 
