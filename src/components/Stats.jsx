de02b4d76dc033681ea2cf0969cb3fba60eb67d2
Jair Neri <yayodcv@gmail.com>
2020-12-23 22:40:56 -0600

    Proyects ready

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const Stats = () => <div className="stats" />;
-
+const Stats = () => (
+  <div className="stats">
+    <h2>Projects (3)</h2>
+    <p>
+      <span>react</span>
+      <span>vue</span>
+      <span className="blue">responsive</span>
+    </p>
+  </div>
+);
 export default Stats;
