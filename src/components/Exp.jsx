205a081dcbfccf2db84dedcb853fa8167a7ac0d2
Jair Neri <yayodcv@gmail.com>
2020-12-23 22:03:18 -0600

    Exp setup structure, styles

--- src/components/Exp.jsx
+++ src/components/Exp.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
-const Exp = () => <div className="exp">5</div>;
+const Proyect = () => (
+  <div className="e-p">
+    <picture>
+      <img src="" alt="logo1" />
+    </picture>
+    <div className="e-d">
+      <small>Feb 2017 - Current</small>
+      <h3>Front-end developer</h3>
+      <p>
+        Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque
+        feugiat malesuada molestie.
+      </p>
+    </div>
+  </div>
+);
+
+const Exp = () => (
+  <div className="exp">
+    <h2>Experiencias </h2>
+    <Proyect />
+    <Proyect />
+  </div>
+);
 
 export default Exp;
