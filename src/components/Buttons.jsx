4e5cb914a2bae9938ef969f006175dbe56930964
Jair Neri <yayodcv@gmail.com>
2020-12-24 13:03:46 -0600

    Buttons - setup estructure, styles

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
 
-const Buttons = () => <div className="buttons" />;
+const Buttons = () => (
+  <div className="buttons">
+    <button type="button">{'<'}</button>
+    <button type="button">1</button>
+    <button type="button" className="blue">
+      2
+    </button>
+    <button type="button">3</button>
+    <button type="button">···</button>
+    <button type="button">10</button>
+    <button type="button">{'>'}</button>
+  </div>
+);
 export default Buttons;
