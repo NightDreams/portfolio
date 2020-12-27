111e1581b87f52c5fd4d50cae052d54ddcd42a79
Jair Neri <yayodcv@gmail.com>
2020-12-26 23:54:00 -0600

    Proyects setup - Data modular

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -4,7 +4,16 @@ import Galery from '../assets/galery.jpg';
 import CockApp from '../assets/cookapp.jpg';
 import Checkout from '../assets/chek2.jpg';
 
-const Proyect = ({ props }) => (
+const pDes = {
+  Recipe:
+    'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
+  Gallery:
+    'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with instagram profile page. The design is from devchallenge.io',
+  Check:
+    'In this project, I work with HTML and CSS to create a responsive page. This page is similiar with a page. The design is from devchallenge.io',
+};
+
+const Proyect = ({ props, t, d }) => (
   <div className="cards">
     <picture>
       <img src={props} alt="img-proyect" />
@@ -14,11 +23,8 @@ const Proyect = ({ props }) => (
       <small>@css</small>
       <small>@responsive</small>
     </span>
-    <h2>proyect</h2>
-    <p>
-      In this project, I work with HTML and CSS to create a responsive page.
-      This page is similiar with a page. The design is from devchallenge.io
-    </p>
+    <h2>{t}</h2>
+    <p>{d}</p>
     <div className="c-b">
       <p className="fill">2</p>
       <p className="outline">2</p>
@@ -28,9 +34,9 @@ const Proyect = ({ props }) => (
 
 const Proyects = () => (
   <div className="proyect">
-    <Proyect props={CockApp} />
-    <Proyect props={Galery} />
-    <Proyect props={Checkout} />
+    <Proyect props={CockApp} t="Recipe Blog" d={pDes.Recipe} />
+    <Proyect props={Galery} t="My Gallery" d={pDes.Gallery} />
+    <Proyect props={Checkout} t="Checkout" d={pDes.Check} />
   </div>
 );
 
